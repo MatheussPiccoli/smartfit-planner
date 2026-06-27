41a552c2bd3cb5f5bd570d018450cb943a228be3
matheusspiccoli <matheuspdmoura@gmail.com>
2026-06-27 14:04:20 -0300

    style: remove unnecessery blank spaces

--- models/treinos.py
+++ models/treinos.py
@@ -3,8 +3,6 @@ from datetime import date
 from sqlalchemy import Column, Integer, Boolean, Date, String, ForeignKey, Enum
 from sqlalchemy.orm import relationship
 from sqlalchemy.types import Uuid
-
-# Importamos a Base e os Enums necessários
 from models.database import Base
 from models.enums import StatusSessao
 
@@ -12,31 +10,25 @@ class PlanoTreino(Base):
     __tablename__ = 'planos_treino'
     
     id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    createdAt = Column(Date, default=date.today)
-    deloadAtivo = Column(Boolean, default=False)
+    dataCriacao = Column(Date, default=date.today)
     semanaAtual = Column(Integer, default=1)
     totalSemanas = Column(Integer, nullable=False)
+    deloadAtivo = Column(Boolean, default=False)
     
-
     aluno_id = Column(Uuid(as_uuid=True), ForeignKey('alunos.id', ondelete="CASCADE"))
-    
-
     aluno = relationship("Aluno", back_populates="planos")
     
     sessoes = relationship("SessaoTreino", back_populates="plano", cascade="all, delete-orphan")
 
-
 class SessaoTreino(Base):
     __tablename__ = 'sessoes_treino'
-
-    id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    data = Column(Date, nullable=True) 
-    duracaoMin = Column(Integer, nullable=True)
-    observacoes = Column(String(500), nullable=True)
-    status = Column(Enum(StatusSessao), default=StatusSessao.PLANEJADA)
-
-    plano_id = Column(Uuid(as_uuid=True), ForeignKey('planos_treino.id', ondelete="CASCADE"))
     
+    id = Column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    dataPlanejada = Column(Date, nullable=True)
+    nome_sessao = Column(String(50)) # Ex: "Peito + Tríceps" (Igual ao protótipo do UC02)
+    status = Column(Enum(StatusSessao), default=StatusSessao.PLANEJADA)
+    
+    plano_id = Column(Uuid(as_uuid=True), ForeignKey('planos_treino.id', ondelete="CASCADE"))
     plano = relationship("PlanoTreino", back_populates="sessoes")
-
+    
     exercicios_planejados = relationship("PlanoExercicio", back_populates="sessao", cascade="all, delete-orphan")
