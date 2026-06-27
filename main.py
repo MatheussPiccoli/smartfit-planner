091001f9fe8e5991f46eb77cbcd72fd8b59d5d5d
matheusspiccoli <matheuspdmoura@gmail.com>
2026-06-27 16:27:21 -0300

    feat: add exercises catalog and load then on first run

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-from models import usuarios, exercicios, treinos
-from models.database import criar_banco_de_dados
+from models.database import SessionLocal, criar_banco_de_dados
+from models.exercicios import Exercicio
+from models.enums import GrupoMuscularEnum
 
-def main():
-    criar_banco_de_dados()
+from models.exercicios_catalogo import obter_catalogo_inicial
+from controllers.usuarios import UsuarioController
+from controllers.treino import TreinoController
+from views.main_view import SmartFitApp
+
+def popular_catalogo_exercicios(db):
+    from models.exercicios import Exercicio
+    if db.query(Exercicio).first():
+        return
+
+    print("Importando catálogo de exercícios...")
+    exercicios = obter_catalogo_inicial()
+    db.add_all(exercicios)
+    db.commit()
+    print(f"[OK] {len(exercicios)} exercícios inseridos!")
 
 if __name__ == "__main__":
-    main()
+    criar_banco_de_dados()
+    db = SessionLocal()
+    
+    popular_catalogo_exercicios(db)
+
+    usuario_ctrl = UsuarioController(db)
+    treino_ctrl = TreinoController(db)
+    
+    # O app inicia vazio, forçando o login e a navegação real!
+    app = SmartFitApp(usuario_ctrl, treino_ctrl)
+    app.mainloop()
